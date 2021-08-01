7e037f20f17722a4dfc012b9c27bd5f44642d3ca
John Oberhauser <j.git-global@obez.io>
2021-08-01 01:29:22 -0500

    screenshots

---
+++ sway/README.md
@@ -0,0 +1,9 @@
+programs required:
+
+rofi - application launcher
+swaylock - lock screen
+grim - screenshot util
+slurp - select screen area for grim
+waybar - top bar
+lxappearance - set gtk window themes
+polkit-gnome - polkit program
--- sway/searchApplications.sh
+++ sway/searchApplications.sh
@@ -13,20 +13,31 @@ listApps() {
   done
   # Custom entries
   echo Pamac
+  echo GIMP
 }
 
 selection="$(listApps | rofi -dmenu -i)"
+echo selected $selection
+
+# Custom entries
+if [[ $selection == "Pamac" ]]; then
+  echo launching org.manjaro.pamac.manager
+  gtk-launch org.manjaro.pamac.manager
+  exit 0
+fi
+if [[ $selection == "GIMP" ]]; then
+  echo launching gimp
+  gtk-launch gimp
+  exit 0
+fi
 
 for path in "${paths[@]}"
 do
   name=$(cat $path | grep -m 1 ^Name= | sed 's/Name=//')
   if [[ $selection == $name ]]; then
     app=$(echo $path | sed 's/.*\///' | sed 's/.desktop//')
+    echo launching $app
     gtk-launch $app
     break
-  fi
-  # Custom entries
-  if [[ $selection == "Pamac" ]]; then
-    gtk-launch org.manjaro.pamac.manager
   fi
 done
