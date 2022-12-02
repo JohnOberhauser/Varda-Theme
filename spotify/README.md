a3dff7dc7219120fe1d81ee678765907dfae79ea
John Oberhauser <j.git-global@obez.io>
2022-12-01 21:37:11 -0600

    spotify stuff

--- spotify/README.md
+++ spotify/README.md
@@ -2,3 +2,5 @@ Theme applied with spicetify.  Follow instructions to install
 https://github.com/khanhas/spicetify-cli
 
 Using spicetify v2
+
+run permissionFix.sh to fix permission issues
---
+++ spotify/permissionFix.sh
@@ -0,0 +1,2 @@
+sudo chmod a+wr /var/lib/flatpak/app/com.spotify.Client/x86_64/stable/active/files/extra/share/spotify
+sudo chmod a+wr -R /var/lib/flatpak/app/com.spotify.Client/x86_64/stable/active/files/extra/share/spotify/Apps
