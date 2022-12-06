d7260442731e316ea0f541e8cbb50d2f8b4d92dd
John Oberhauser <j.git-global@obez.io>
2022-12-06 00:36:10 -0600

    saving

--- hypr/scripts/powerOptions/lock.sh
+++ hypr/scripts/powerOptions/lock.sh
@@ -2,7 +2,7 @@
 
 nohup swayidle timeout 20 'hyprctl dispatch dpms off' \
     resume 'hyprctl dispatch dpms on' \
-    &
+    2>&1 /dev/null &
 
 swaylock -C $HOME/.config/hypr/components/swaylock/config
 
