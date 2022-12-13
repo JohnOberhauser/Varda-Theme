027e584303fc6897e00659134674516ed2637134
John Oberhauser <j.git-global@obez.io>
2022-12-12 21:28:23 -0600

    system menu

---
+++ hypr/scripts/systemMenu/toggleSystemMenu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+LOCK_FILE="$HOME/.cache/eww-system-menu.lock"
+
+if [[ ! -f "$LOCK_FILE" ]]; then
+    touch "$LOCK_FILE"
+    eww -c $HOME/.config/hypr/components/eww open systemMenuWindow
+else
+    eww -c $HOME/.config/hypr/components/eww close systemMenuWindow
+    rm "$LOCK_FILE"
+fi
