3bc7926278c03b644ea0c82c29520f1d8594d38a
John Oberhauser <j.git-global@obez.io>
2023-06-19 21:34:09 -0500

    proton vpn in firefox icons, screenshare fixes

--- hypr/scripts/video/checkIfScreenSharing.sh
+++ hypr/scripts/video/checkIfScreenSharing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-portalOutput=$(wpctl status | sed -n '/Sinks:/,$p' | grep xdg-desktop-portal-wlr)
+portalOutput=$(wpctl status | sed -n '/Sinks:/,$p' | grep xdg-desktop-portal-hyprland)
 
 if [[ $portalOutput == "" ]]; then
     eww -c $HOME/.config/hypr/components/eww update screen_sharing_reveal=false
