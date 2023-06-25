b437a4cb0cb49dbed2ef15b801e5cbb9eb5279e4
John Oberhauser <j.git-global@obez.io>
2023-06-24 22:21:33 -0500

    performance fix

--- hypr/scripts/audio/showVolumeAlert.sh
+++ hypr/scripts/audio/showVolumeAlert.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 eww -c $HOME/.config/hypr/components/eww update volumeAlertLastUpdateTimeStamp=$(($(date +%s%N)/1000000))
+
+LOCK_FILE="$HOME/.cache/eww-volume-alert.lock"
+
+if [[ -f "$LOCK_FILE" ]]; then
+    exit
+fi
+
 eww -c $HOME/.config/hypr/components/eww open volumeAlertWindow
 
 $HOME/.config/hypr/scripts/audio/closeVolumeAlert.sh
