9de91fd1ae24609d00babb2dc00e579c01788e11
John Oberhauser <j.git-global@obez.io>
2023-09-27 16:54:07 -0500

    fixing screen share indicator bug

--- hypr/scripts/video/checkIfScreenSharing.sh
+++ hypr/scripts/video/checkIfScreenSharing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-portalOutput=$(wpctl status | sed -n '/Sinks:/,$p' | grep xdg-desktop-portal-hyprland)
+portalOutput=$(wpctl status | sed -n '/Video/,$p' | sed -n '/Streams:/,$p' | grep xdg-desktop-portal-hyprland)
 
 if [[ $portalOutput == "" ]]; then
     eww -c $HOME/.config/hypr/components/eww update screen_sharing_reveal=false
