eb36a977c47c444363b3fa5760328d8866bef89a
John Oberhauser <j.git-global@obez.io>
2023-10-14 13:46:18 -0500

    bluetooth

---
+++ hypr/components/rofi/bluetooth/README.md
@@ -0,0 +1,4 @@
+The shell script comes from
+https://github.com/nickclyde/rofi-bluetooth/tree/master
+
+Edited the script to add a quick connect option
--- hypr/scripts/audio/swapOutputToBluetooth.sh
+++ hypr/scripts/audio/swapOutputToBluetooth.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sleep 3
+#sleep 3
+
+echo "Swapping output to bluetooth..."
 
 BLUETOOTH_ID=$(wpctl status | sed -n '/Sinks:/,$p' | awk '1;/Sink endpoints:/{exit}' | tail -n +2 | head -n -2 | grep 'Buds' | awk '{print $2}' | awk -F "." '{print $1}')
 
