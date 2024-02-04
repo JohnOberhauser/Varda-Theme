0ee8b7eaf1de0d175e7445d485a015e2e45440c4
John Oberhauser <j.git-global@obez.io>
2024-02-03 23:10:51 -0600

    battery icon

---
+++ hypr/scripts/battery/getBatteryIcon.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+battery=$(acpi -b | awk -F ", " '{print $2}' | awk -F "%" '{print $1}')
+
+isCharging=$(acpi -b | grep Charging)
+
+if [[ $isCharging == *"Charging"* ]]; then
+    if [[ $battery -le 10 ]]; then
+        echo "󰢜"
+    elif [[ $battery -le 20 ]]; then
+        echo "󰂆"
+    elif [[ $battery -le 30 ]]; then
+        echo "󰂇"
+    elif [[ $battery -le 40 ]]; then
+        echo "󰂈"
+    elif [[ $battery -le 50 ]]; then
+        echo "󰢝"
+    elif [[ $battery -le 60 ]]; then
+        echo "󰂉"
+    elif [[ $battery -le 70 ]]; then
+        echo "󰢞"
+    elif [[ $battery -le 80 ]]; then
+        echo "󰂊"
+    elif [[ $battery -le 90 ]]; then
+        echo "󰂋"
+    else
+        echo "󰂅"
+    fi
+else
+    if [[ $battery -le 10 ]]; then
+        echo "󰁺"
+    elif [[ $battery -le 20 ]]; then
+        echo "󰁻"
+    elif [[ $battery -le 30 ]]; then
+        echo "󰁼"
+    elif [[ $battery -le 40 ]]; then
+        echo "󰁽"
+    elif [[ $battery -le 50 ]]; then
+        echo "󰁾"
+    elif [[ $battery -le 60 ]]; then
+        echo "󰁿"
+    elif [[ $battery -le 70 ]]; then
+        echo "󰂀"
+    elif [[ $battery -le 80 ]]; then
+        echo "󰂁"
+    elif [[ $battery -le 90 ]]; then
+        echo "󰂂"
+    else
+        echo "󰁹"
+    fi
+fi
+
+
