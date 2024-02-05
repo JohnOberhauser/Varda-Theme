ebd66770d798ad2da96cbde80b17a93fd2233af1
John Oberhauser <j.git-global@obez.io>
2024-02-04 21:43:14 -0600

    screenshot fixture

--- hypr/scripts/screenshots/captureArea.sh
+++ hypr/scripts/screenshots/captureArea.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-grim -g "$(slurp)" $(xdg-user-dir PICTURES)/Screenshots/$(date +'%s_grim.png')
+grim -g "$(slurp)" $HOME/Pictures/Screenshots/$(date +'%s_grim.png')
 
 play $HOME/.config/hypr/assets/sounds/camera-shutter.ogg
