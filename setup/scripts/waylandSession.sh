dce24ad7afa99ed702897d024bd6e93df049537b
John Oberhauser <j.git-global@obez.io>
2024-02-12 19:47:31 -0600

    fix

--- setup/scripts/waylandSession.sh
+++ setup/scripts/waylandSession.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo cp $HOME/workspace/Varda-theme/hypr/setup/hyprland-wrapper.desktop /user/share/wayland-sessions/
-sudo cp $HOME/workspace/Varda-theme/hypr/setup/hyprland-wrapper.sh /user/local/bin/
+sudo cp $HOME/workspace/Varda-theme/hypr/setup/hyprland-wrapper.desktop /usr/share/wayland-sessions/
+sudo cp $HOME/workspace/Varda-theme/hypr/setup/hyprland-wrapper.sh /usr/local/bin/
