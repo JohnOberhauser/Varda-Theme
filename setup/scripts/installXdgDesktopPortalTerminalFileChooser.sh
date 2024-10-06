d87b2f7777dbc22125caf0884f585f4ee2b7e974
John Oberhauser <j.git-global@obez.io>
2024-10-06 18:07:16 -0500

    getting terminal file picker to work

--- setup/scripts/installXdgDesktopPortalTerminalFileChooser.sh
+++ setup/scripts/installXdgDesktopPortalTerminalFileChooser.sh
@@ -7,3 +7,15 @@ git clone git@github.com:JohnOberhauser/xdg-desktop-portal-termfilechooser.git
 
 cd xdg-desktop-portal-termfilechooser
 meson build && ninja -C build && ninja -C build install
+
+mkdir -p $HOME/.config/xdg-desktop-portal
+cp $HOME/workspace/Varda-Theme/setup/portals.conf $HOME/.config/xdg-desktop-portal/
+
+sudo mv /usr/local/share/xdg-desktop-portal/portals/termfilechooser.portal /usr/share/xdg-desktop-portal/portals/
+sudo mv /usr/local/lib/systemd/user/xdg-desktop-portal-termfilechooser.service /usr/lib/systemd/user/
+sudo mv /usr/local/share/dbus-1/services/org.freedesktop.impl.portal.desktop.termfilechooser.service /usr/share/dbus-1/services/
+
+mkdir -p $HOME/.config/xdg-desktop-portal-termfilechooser
+cp -r /usr/local/share/xdg-desktop-portal-termfilechooser/* $HOME/.config/xdg-desktop-portal-termfilechooser/
+
+systemctl --user restart xdg-desktop-portal.service
