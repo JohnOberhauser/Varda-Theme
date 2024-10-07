1518aad815dfd8646c50108999f365034a82c268
John Oberhauser <j.git-global@obez.io>
2024-10-07 12:31:46 -0500

    install update

--- setup/scripts/installGtkThemes.sh
+++ setup/scripts/installGtkThemes.sh
@@ -6,7 +6,7 @@ cd $HOME/Downloads
 mkdir -p $HOME/.themes
 mkdir -p $HOME/.icons
 
-wget -O everforest-theme.zip https://github.com/Fausto-Korpsvart/Everforest-GTK-Theme/archive/refs/heads/master.zip && unzip -q -o everforest-theme.zip -d ~/Downloads && rm everforest-theme.zip
+wget -O everforest-theme.zip https://github.com/Fausto-Korpsvart/Everforest-GTK-Theme/archive/refs/heads/master.zip && aunpack everforest-theme.zip && rm everforest-theme.zip
 wget -O Nordzy.tar.gz https://github.com/alvatip/Nordzy-icon/releases/download/1.8.7/Nordzy.tar.gz && aunpack Nordzy.tar.gz && rm Nordzy.tar.gz
 
 cp -r $HOME/Downloads/Everforest-GTK-Theme-master/icons/Everforest-Dark $HOME/.icons/
