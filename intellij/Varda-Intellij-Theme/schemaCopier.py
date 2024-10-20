484016a5760e9f99ef69cebe20695b4aa4a6e206
John Oberhauser <j.git-global@obez.io>
2024-10-20 16:46:25 -0400

    everforest intellij theme progress

--- intellij/Varda-Intellij-Theme/schemaCopier.py
+++ intellij/Varda-Intellij-Theme/schemaCopier.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 def replace_text_in_file(input_file, output_file):
     try:
@@ -6,30 +7,34 @@ def replace_text_in_file(input_file, output_file):
             file_data = file.read()
 
         # Replace the text
-        file_data = file_data.replace("Varda", "Everforest")
+        file_data = re.sub(r"Varda", "Everforest", file_data, flags=re.IGNORECASE)
         # white
-        file_data = file_data.replace("D0EBEE", "D3C6AA")
+        file_data = re.sub(r"D0EBEE", "D3C6AA", file_data, flags=re.IGNORECASE)
         # black
-        file_data = file_data.replace("0C0E11", "1E2326")
+        file_data = re.sub(r"0C0E11", "1E2326", file_data, flags=re.IGNORECASE)
+        file_data = re.sub(r"C0E11", "1E2326", file_data, flags=re.IGNORECASE)
         # greys
-        file_data = file_data.replace("121519", "272E33")
-        file_data = file_data.replace("171B21", "2E383C")
-        file_data = file_data.replace("1C2128", "374145")
-        file_data = file_data.replace("222830", "414B50")
-        file_data = file_data.replace("272E38", "495156")
-        file_data = file_data.replace("38414F", "4F5B58")
+        file_data = re.sub(r"121519", "272E33", file_data, flags=re.IGNORECASE)
+        file_data = re.sub(r"171B21", "2E383C", file_data, flags=re.IGNORECASE)
+        file_data = re.sub(r"1C2128", "374145", file_data, flags=re.IGNORECASE)
+        file_data = re.sub(r"222830", "414B50", file_data, flags=re.IGNORECASE)
+        file_data = re.sub(r"272E38", "495156", file_data, flags=re.IGNORECASE)
+        file_data = re.sub(r"38414F", "4F5B58", file_data, flags=re.IGNORECASE)
         # primary
-        file_data = file_data.replace("52677C", "A7C080")
+        file_data = re.sub(r"52677C", "A7C080", file_data, flags=re.IGNORECASE)
         # primary alt
-        file_data = file_data.replace("8295A9", "7A8478")
+        file_data = re.sub(r"8295A9", "7A8478", file_data, flags=re.IGNORECASE)
         # secondary
-        file_data = file_data.replace("665276", "D699B6")
+        file_data = re.sub(r"665276", "D699B6", file_data, flags=re.IGNORECASE)
         # tertiary / warning / yellow
-        file_data = file_data.replace("C78C56", "DBBC7F")
+        file_data = re.sub(r"C78C56", "DBBC7F", file_data, flags=re.IGNORECASE)
         # error / red
-        file_data = file_data.replace("733447", "E67E80")
+        file_data = re.sub(r"733447", "E67E80", file_data, flags=re.IGNORECASE)
         # success / green
-        file_data = file_data.replace("257B76", "83C092")
+        file_data = re.sub(r"257B76", "83C092", file_data, flags=re.IGNORECASE)
+        # VCS modified
+        file_data = re.sub(r'("FILESTATUS_MODIFIED" value=")(52677c)', r'\1 7FBBB3', file_data, flags=re.IGNORECASE)
+
 
         # Write the new content to the output file
         with open(output_file, 'w') as file:
