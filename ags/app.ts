1d05d36a9e879aad8606f6b01557e1dca4022820
John Oberhauser <j.git-global@obez.io>
2024-11-28 11:13:49 -0500

    ags v2 sidebar

--- ags/app.ts
+++ ags/app.ts
@@ -4,13 +4,20 @@ import TopBar from "./widget/bar/TopBar"
 import Calendar from "./widget/calendar/Calendar"
 import SystemMenuWindow from "./widget/systemMenu/SystemMenuWindow";
 import {BrightnessAlert, VolumeAlert} from "./widget/alerts/Alerts";
+import SideBar from "./widget/bar/SideBar";
 
 App.start({
     css: style,
-    main() {
-        TopBar()
-        Calendar(Astal.WindowAnchor.TOP)
-        SystemMenuWindow(Astal.WindowAnchor.TOP | Astal.WindowAnchor.RIGHT)
+    main(...args: Array<string>) {
+        if (args.includes("sidebar")) {
+            SideBar()
+            Calendar(Astal.WindowAnchor.BOTTOM | Astal.WindowAnchor.LEFT)
+            SystemMenuWindow(Astal.WindowAnchor.TOP | Astal.WindowAnchor.LEFT)
+        } else {
+            TopBar()
+            Calendar(Astal.WindowAnchor.TOP)
+            SystemMenuWindow(Astal.WindowAnchor.TOP | Astal.WindowAnchor.RIGHT)
+        }
         VolumeAlert()
         BrightnessAlert()
     },
---
+++ ags/widget/bar/SideBar.tsx
@@ -0,0 +1,50 @@
+import {App, Astal, Gtk} from "astal/gtk3"
+import {
+    BatteryButton,
+    BluetoothButton,
+    ClockButton,
+    MenuButton,
+    MicrophoneButton,
+    NetworkButton,
+    ScreenRecordingButton,
+    VolumeButton,
+    Workspaces
+} from "./BarWidgets";
+
+export default function () {
+    let iconCss = "margin-top: 10px;"
+
+    return <window
+        className="window"
+        monitor={0}
+        exclusivity={Astal.Exclusivity.EXCLUSIVE}
+        margin={5}
+        anchor={Astal.WindowAnchor.TOP
+            | Astal.WindowAnchor.LEFT
+            | Astal.WindowAnchor.BOTTOM}
+        application={App}>
+        <centerbox
+            vertical={true}
+            css={`
+                padding: 2px;
+                min-width: 40px;
+            `}>
+            <box vertical={true}>
+                <MenuButton css={"padding-top: 6px;"}/>
+                <Workspaces vertical={true}/>
+            </box>
+            <box/>
+            <box
+                vertical={true}
+                valign={Gtk.Align.END}>
+                <ScreenRecordingButton css={iconCss}/>
+                <VolumeButton css={iconCss}/>
+                <MicrophoneButton css={iconCss}/>
+                <BluetoothButton css={iconCss}/>
+                <NetworkButton css={iconCss}/>
+                <BatteryButton css={iconCss}/>
+                <ClockButton css={"margin-top: 10px; padding-bottom: 6px;"} singleLine={false}/>
+            </box>
+        </centerbox>
+    </window>
+}
