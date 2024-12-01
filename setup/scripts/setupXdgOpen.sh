2727b9d4d08afe6258b050c47c5e56340e290075
John Oberhauser <j.git-global@obez.io>
2024-11-30 19:23:32 -0600

    xdg open update

--- setup/scripts/setupXdgOpen.sh
+++ setup/scripts/setupXdgOpen.sh
@@ -5,3 +5,5 @@ xdg-mime default org.gnome.Loupe.desktop image/jpeg
 xdg-mime default org.gnome.Loupe.desktop image/jpg
 xdg-mime default org.gnome.Loupe.desktop image/gif
 xdg-mime default firefox-nightly.desktop application/pdf
+xdg-mime default firefox-nightly.desktop x-scheme-handler/http
+xdg-mime default firefox-nightly.desktop x-scheme-handler/https
