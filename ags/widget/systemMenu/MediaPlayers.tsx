81019d9e5228538b858d3cf37f69a5a2721dbe3c
John Oberhauser <j.git-global@obez.io>
2024-12-15 21:55:01 -0600

    media player ags update

--- ags/widget/systemMenu/MediaPlayers.tsx
+++ ags/widget/systemMenu/MediaPlayers.tsx
@@ -1,6 +1,6 @@
-import { Gtk } from "astal/gtk3"
+import {Gtk} from "astal/gtk3"
 import Mpris from "gi://AstalMpris"
-import { bind, Variable } from "astal"
+import {bind, Variable} from "astal"
 
 function lengthStr(length: number) {
     const min = Math.floor(length / 60)
@@ -38,10 +38,12 @@ function MediaPlayer({ player }: { player: Mpris.Player }) {
         vertical={true}>
         <label
             className="labelSmallBold"
+            truncate={true}
             halign={CENTER}
             label={title}/>
         <label
             className="labelSmall"
+            truncate={true}
             halign={CENTER}
             label={artist}/>
         <box
@@ -76,6 +78,23 @@ function MediaPlayer({ player }: { player: Mpris.Player }) {
             halign={CENTER}>
             <button
                 className="controlButton"
+                onClicked={() => {
+                    if (player.shuffleStatus === Mpris.Shuffle.ON) {
+                        player.set_shuffle_status(Mpris.Shuffle.OFF)
+                    } else {
+                        player.set_shuffle_status(Mpris.Shuffle.ON)
+                    }
+                }}
+                visible={bind(player, "shuffleStatus").as((shuffle) => shuffle !== Mpris.Shuffle.UNSUPPORTED)}
+                label={bind(player, "shuffleStatus").as((shuffle) => {
+                    if (shuffle === Mpris.Shuffle.ON) {
+                        return ""
+                    } else {
+                        return "󰒞"
+                    }
+                })}/>
+            <button
+                className="controlButton"
                 onClicked={() => player.previous()}
                 visible={bind(player, "canGoPrevious")}
                 label=""/>
@@ -89,6 +108,27 @@ function MediaPlayer({ player }: { player: Mpris.Player }) {
                 onClicked={() => player.next()}
                 visible={bind(player, "canGoNext")}
                 label=""/>
+            <button
+                className="controlButton"
+                onClicked={() => {
+                    if (player.loopStatus === Mpris.Loop.NONE) {
+                        player.set_loop_status(Mpris.Loop.PLAYLIST)
+                    } else if (player.loopStatus === Mpris.Loop.PLAYLIST) {
+                        player.set_loop_status(Mpris.Loop.TRACK)
+                    } else {
+                        player.set_loop_status(Mpris.Loop.NONE)
+                    }
+                }}
+                visible={bind(player, "loopStatus").as((status) => status !== Mpris.Loop.UNSUPPORTED)}
+                label={bind(player, "loopStatus").as((status) => {
+                    if (status === Mpris.Loop.NONE) {
+                        return "󰑗"
+                    } else if (status === Mpris.Loop.PLAYLIST) {
+                        return "󰑖"
+                    } else {
+                        return "󰑘"
+                    }
+                })}/>
         </box>
     </box>
 }
