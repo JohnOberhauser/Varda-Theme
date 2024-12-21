0fa77edef6d54f8a7eb3afa5a90ac1b985e6bd9c
John Oberhauser <j.git-global@obez.io>
2024-12-21 16:07:35 -0600

    vpn update

--- setup/scripts/setupVpnConnections.sh
+++ setup/scripts/setupVpnConnections.sh
@@ -4,8 +4,5 @@ USERNAME="$1"
 PASS="$2"
 
 for f in  ../openvpn/*.ovpn; do
-  nmcli c i type openvpn file "$f"
-  nmcli c mod "$(basename -s .ovpn "$f")" \
-    +vpn.data "connection-type=password-tls, username=$USERNAME" \
-    vpn.secrets "password=$PASS"
+  ./addVpnConnection.sh $f $1 $2
 done
