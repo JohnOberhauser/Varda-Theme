f94f95ae046b0f6dc7d7f2b41304fc3cf1b7f4e0
John Oberhauser <j.git-global@obez.io>
2024-12-21 12:15:56 -0600

    updating workspace icons in bar

--- ags/widget/bar/BarWidgets.tsx
+++ ags/widget/bar/BarWidgets.tsx
@@ -12,27 +12,58 @@ import {SystemMenuWindowName} from "../systemMenu/SystemMenuWindow";
 import Bluetooth from "gi://AstalBluetooth"
 import {activeVpnConnections} from "../systemMenu/NetworkControls";
 import {isRecording} from "../screenshot/Screenshot";
+import Divider from "../common/Divider";
+
+function groupByProperty(
+    array: Hyprland.Workspace[],
+): Hyprland.Workspace[][] {
+    const map = new Map<Hyprland.Monitor, Hyprland.Workspace[]>();
+
+    array.forEach((item) => {
+        const key = item.monitor;
+        if (key === null) {
+            return
+        }
+        if (!map.has(key)) {
+            map.set(key, []);
+        }
+        map.get(key)!.unshift(item);
+    });
+
+    return Array.from(map.values()).sort((a, b) => {
+        return a[0].monitor.id - b[0].monitor.id
+    });
+}
 
 export function Workspaces({vertical}: { vertical: boolean }) {
     const hypr = Hyprland.get_default()
 
     return <box
-        vertical={vertical}
-    >
-        {Array.from({length: 6}, (_, i) => (
-            <button
-                label={
-                    bind(hypr, "focusedWorkspace").as(focusedWs =>
-                        focusedWs.get_id() == i + 1 ? "" : ""
-                    )
-                }
-                className="iconButton"
-                onClicked={() => {
-                    hypr.dispatch("workspace", `${i + 1}`)
-                }}>
-
-            </button>
-        ))}
+        vertical={vertical}>
+        {bind(hypr, "workspaces").as((workspaces) => {
+            const groupedWorkspaces = groupByProperty(workspaces)
+            return groupedWorkspaces.map((workspaceGroup, index) => {
+                return <box
+                    vertical={vertical}>
+                    {index > 0 && index < groupedWorkspaces.length && <Divider/>}
+                    {workspaceGroup.sort((a, b) => {
+                        return a.id - b.id
+                    }).map((workspace) => {
+                        return <button
+                            label={
+                                bind(workspace.monitor, "activeWorkspace").as((activeWorkspace) =>
+                                    activeWorkspace.id == workspace.id ? "" : ""
+                                )
+                            }
+                            className="iconButton"
+                            onClicked={() => {
+                                hypr.dispatch("workspace", `${workspace.id}`)
+                            }}>
+                        </button>
+                    })}
+                </box>
+            })
+        })}
     </box>
 }
 
