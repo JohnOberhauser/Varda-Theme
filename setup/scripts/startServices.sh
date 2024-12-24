d26dc87d852a2222c7634930d189a437f5499544
John Oberhauser <j.git-global@obez.io>
2024-12-23 18:07:21 -0600

    updating setup script for hyprpolkitagent

--- setup/scripts/startServices.sh
+++ setup/scripts/startServices.sh
@@ -3,3 +3,4 @@
 sudo systemctl enable --now bluetooth.service
 sudo systemctl enable --now ufw.service
 systemctl --user enable --now hypridle.service
+systemctl --user enable --now hyprpolkitagent.service
