4cd6482b49ff1736e6912cc739f09bd3fd339162
John Oberhauser <j.git-global@obez.io>
2025-01-03 23:47:23 -0600

    updating keymaps and bookmarks

--- firefox/startpage/scripts.js
+++ firefox/startpage/scripts.js
@@ -43,7 +43,7 @@ const bookmarks = [
   },
   {"id":"qjTUGL2OWFspQmm2","label":"Video","bookmarks":[
       {"id":"pZDB2ilcq8t0m6eW","label":"YouTube","url":"https://www.youtube.com/"},
-      {"id":"5YRKKQap76zg1ZxO","label":"Twitch","url":"https://www.twitch.tv/directory"},
+      {"id":"5YRKKQap76zg1ZxO","label":"Jellyfin","url":"http://192.168.50.158:8096"},
       {"id":"ereuzztrzJbI0lqk","label":"Netflix","url":"https://www.netflix.com/browse"},
       {"id":"R37Y4hMSIvTGhRmy","label":"Max","url":"https://play.max.com/"}
     ]
