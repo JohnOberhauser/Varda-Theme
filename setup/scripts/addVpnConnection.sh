77a89a7664ac3dad56901088a092ba39f2cc8b77
John Oberhauser <j.git-global@obez.io>
2025-01-06 19:02:53 -0600

    updatin vpn scripts

--- setup/scripts/addVpnConnection.sh
+++ setup/scripts/addVpnConnection.sh
@@ -4,6 +4,17 @@ FILE="$1"
 USERNAME="$2"
 PASS="$3"
 
+usage() {
+  echo "Usage: $0 [file_path] [username] [password]"
+  exit 1
+}
+
+if [[ -z $FILE || -z $USERNAME || -z $PASS ]]; then
+  echo "Error: Missing required parameters."
+  usage
+  exit 1
+fi
+
 nmcli c i type openvpn file "$1"
 nmcli c mod "$(basename -s .ovpn "$1")" \
   +vpn.data "connection-type=password-tls, username=$USERNAME" \
