3f1f6c36cdbe9309805ffad95048cfd517fb3e07
John Oberhauser <j.git-global@obez.io>
2026-03-08 14:03:34 -0500

    keyboard wakeup service

--- setup/scripts/startServices.sh
+++ setup/scripts/startServices.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+sudo cp $HOME/workspace/Varda-Theme/setup/installFiles/keyboard-wakeup.service /etc/systemd/system/
+
+sudo systemctl enable --now keyboard-wakeup.service
+
 sudo systemctl enable --now bluetooth.service
 systemctl --user enable --now hypridle.service
